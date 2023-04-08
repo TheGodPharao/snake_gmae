b795a766caf14a5d149ab4f5f6863198b447e6e4
TheGodPharao <125736338+TheGodPharao@users.noreply.github.com>
2023-04-09 01:45:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("The Ultimate Snake Game")
+
+
+
+
+
+
+
+screen.exitonclick()
